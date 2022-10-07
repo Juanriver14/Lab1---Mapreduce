d9dfa28acc9c25fe1938046a28a8825ca08df2d1
Juanriver14 <104448547+Juanriver14@users.noreply.github.com>
2022-10-07 16:52:16 -0400

    Update reducer-1.py

--- mapreduce-test-python/wordcount3/reducer-1.py
+++ mapreduce-test-python/wordcount3/reducer-1.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
+#!/usr/bin/python3
 from operator import itemgetter
 import sys
 
-current_word = None
-current_count = 0
-word = None
-
+reducer_1_output = {}
 for line in sys.stdin:
-    line = line.strip()
-    word, count = line.split('\t', 1)
-    try:
-        count = int(count)
-    except ValueError:
-        continue
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            print "%s\t%s" % (current_word, current_count)
-        current_count = count
-        current_word = word
+  line = line.strip()
+  hour_ip, num = line.split("\t")
+  num = int(num)
 
-if word == current_word:
-    print "%s\t%s" % (current_word, current_count)
+  reducer_1_output[hour_ip] = reducer_1_output.get(hour_ip,0) + num
+sorted_reducer_1_output = sorted(reducer_1_output.items(), key=itemgetter(0))
+
+for hour_ip, num in sorted_reducer_1_output:
+  print("{}\t{}\".format9=(hour_ip, num))
