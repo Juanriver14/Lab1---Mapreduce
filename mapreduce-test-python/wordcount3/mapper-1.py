109b6b0503fa0a425c231ae6a0d62a7c649b2452
Juanriver14 <104448547+Juanriver14@users.noreply.github.com>
2022-10-07 16:40:13 -0400

    Update mapper-1.py

--- mapreduce-test-python/wordcount3/mapper-1.py
+++ mapreduce-test-python/wordcount3/mapper-1.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+#!/usr/bin/python3
+
+from operator import itemgetter
 import sys
+hours = []
+
 for line in sys.stdin:
-    line = line.strip()
-    words = line.split()
-    for word in words:
-        print "%s\t%s" % (word, 1)
+    hour_ip.num = line.split("\t")
+    hours = hour_ip[:7]; ip = hour_ip[7:]
+   print('{}\t{}'.format(hours, ip + ', ' + num))
