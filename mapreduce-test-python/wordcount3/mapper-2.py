80aceb1275a15f3fcaa530b2e542cc107d03c332
Juanriver14 <104448547+Juanriver14@users.noreply.github.com>
2022-10-07 16:58:36 -0400

    Update mapper-2.py

--- mapreduce-test-python/wordcount3/mapper-2.py
+++ mapreduce-test-python/wordcount3/mapper-2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+#!/usr/bin/python3
+
+from operator import itemgetter
 import sys
 
 for line in sys.stdin:
-    line = line.strip()
-    word, frequency = line.split('\t', 1)
-    print "%s\t%s" % (frequency, word)
+  hour_ip,num = line.split("\t")
+  hours = hour_ip[:7]; ip = hour_ip[7:]
+  print('{}\t{}'.format(hour_ip, ip + ', ' + num))
